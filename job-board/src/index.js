2852dc4027df60bd1c49d65fc2a9b4f65c0644d1
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-29 16:35:55 -0400

    Updated index.js for newer version of react-dom

--- job-board/src/index.js
+++ job-board/src/index.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import Login from './components/Login';
-
-const { Switch } = require('react-router-dom');
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Login from "./components/Login";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route path="/" exact component={App} />
-        <Route path="/login" component={Login} />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
         {/* Add more routes for other components as needed */}
-      </Switch>
+      </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
