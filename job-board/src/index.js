08cf054e437ea69c1ace7e5861f84b54221c9770
MartinCespedes <martin.cespedes002@mymdc.net>
2023-04-01 17:14:23 -0400

    Edited index.js , replaced switch with routes

--- job-board/src/index.js
+++ job-board/src/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Login from "./components/Login";
 import JobList from "./components/JobList";
 import JobForm from "./components/JobForm";
 import Profile from "./components/Profile";
-import AdminDashboard from "./components/AdminDashboard";
+import Adminboard from "./components/Adminboard";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Switch>
-        <Route path="/" exact component={App} />
-        <Route path="/login" component={Login} />
-        <Route path="/jobs" component={JobList} />
-        <Route path="/post-job" component={JobForm} />
-        <Route path="/profile" component={Profile} />
-        <Route path="/admin" component={AdminDashboard} />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/jobs" element={<JobList />} />
+        <Route path="/post-job" element={<JobForm />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/admin" element={<Adminboard />} />
         {/* Add more routes for other components as needed */}
-      </Switch>
+      </Routes>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
