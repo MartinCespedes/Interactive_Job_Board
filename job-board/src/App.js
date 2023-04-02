761d5fd2d8bb5c2d515f3315fc380d7d0e7f52a9
MartinCespedes <martin.cespedes002@mymdc.net>
2023-04-02 10:30:19 -0400

    Added Nav bar to App.js

--- job-board/src/App.js
+++ job-board/src/App.js
@@ -4,27 +4,39 @@ import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Interactive Job Board</h1>
+      <header className="App-header bg-blue-500 text-white">
+        <h1 className="text-2xl font-bold p-4">Interactive Job Board</h1>
         <nav>
-          <ul>
+          <ul className="flex justify-around items-center p-4">
             <li>
-              <Link to="/">Home</Link>
+              <Link to="/" className="hover:text-yellow-400">
+                Home
+              </Link>
             </li>
             <li>
-              <Link to="/login">Login</Link>
+              <Link to="/login" className="hover:text-yellow-400">
+                Login
+              </Link>
             </li>
             <li>
-              <Link to="/jobs">Jobs</Link>
+              <Link to="/jobs" className="hover:text-yellow-400">
+                Jobs
+              </Link>
             </li>
             <li>
-              <Link to="/post-job">Post a Job</Link>
+              <Link to="/post-job" className="hover:text-yellow-400">
+                Post a Job
+              </Link>
             </li>
             <li>
-              <Link to="/profile">Profile</Link>
+              <Link to="/profile" className="hover:text-yellow-400">
+                Profile
+              </Link>
             </li>
             <li>
-              <Link to="/admin">Admin Dashboard</Link>
+              <Link to="/admin" className="hover:text-yellow-400">
+                Admin Dashboard
+              </Link>
             </li>
             {/* more links if needed */}
           </ul>
