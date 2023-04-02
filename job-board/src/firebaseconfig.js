88a61d273764c67c616b1b4e6b82ce0922908304
MartinCespedes <martin.cespedes002@mymdc.net>
2023-04-02 12:02:50 -0400

    Added Firebase configuration, AuthContext.js

---
+++ job-board/src/firebaseconfig.js
@@ -0,0 +1,15 @@
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+
+// const firebaseConfig = {
+//   apiKey: 'API_KEY',
+//   authDomain: 'AUTH_DOMAIN',
+//   projectId: 'PROJECT_ID',
+//   storageBucket: 'STORAGE_BUCKET',
+//   messagingSenderId: 'MESSAGING_SENDER_ID',
+//   appId: 'APP_ID',
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// export const auth = firebase.auth();
