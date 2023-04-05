7c0376d5ddaee67ffbd015b6dd8c70f341567201
MartinCespedes <martin.cespedes002@mymdc.net>
2023-04-04 20:18:43 -0400

    Edited App.js, now search bar for city and job now display

--- job-board/src/App.js
+++ job-board/src/App.js
@@ -63,10 +63,39 @@ function App() {
                 Admin Dashboard
               </NavLink>
             </li>
-            {/* more links if needed */}
           </ul>
         </nav>
-      </header>
+        </header>
+        <main className="flex flex-col items-center justify-start h-screen bg-white text-black pt-16">
+  <div className="text-center p-4">
+    <h2 className="text-3xl font-semibold mb-4">Find Your Dream Job Today</h2>
+    <p className="text-lg mb-6">Search thousands of job listings and find the perfect fit for you.</p>
+    <div className="w-full sm:w-3/4 lg:w-1/2">
+      <form className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
+              <div className="flex-1">
+                <input
+                  type="text"
+                  className="w-full p-2 h-10 border-2 border-black rounded-md focus:outline-none focus:border-blue-300"
+                  placeholder="Job title, keywords or company"
+                />
+              </div>
+              <div className="flex-1">
+                <input
+                  type="text"
+                  className="w-full p-2 h-10 border-2 border-black rounded-md focus:outline-none focus:border-blue-300"
+                  placeholder="City, state, zip or remote"
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-black text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-300 hover:text-black"
+              >
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
